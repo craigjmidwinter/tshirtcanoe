9316d9469a77b435886ecaaf590750f17edea39d
nico.richard <n.richard@tippingcanoe.com>
2017-05-08 15:56:15 -0500

    take that shirt off boi

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ Route::get('/', 'TshirtController@index');
 Route::any('/register', 'TshirtController@register')->name('register');
 Route::get('/tshirt/{user_code}', 'TshirtController@admin')->name('useradmin');
 Route::get('/tshirt/{user_code}/add', 'TshirtController@addShirt');
+Route::get('/tshirt/{user_code}/remove', 'TshirtController@removeShirt');
 Route::get('/tshirt/{admin_code}/approve/{user_code}', 'TshirtController@approve');
 Route::post('/tshirt/{user_code}/update_avatar', 'TshirtController@updateAvatar');
