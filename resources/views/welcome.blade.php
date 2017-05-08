35f3fb592eb2733d26fd858c4d4506045018a776
Craig J. Ward <ward.craig.j@gmail.com>
2017-05-08 11:21:21 -0500

    fix some fugly

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <title>Laravel</title>
+        <title>TShirt Tracker</title>
 
         <!-- Fonts -->
 
@@ -46,7 +46,7 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 34px;
             }
 
             .links > a {
